e9110c54f83cce674a6d906cc88b2366ddab012d
yihhao wang <2633565580@qq.com>
2024-07-09 15:19:24 +0800

    refactor(locale): optimize locale setting

--- src/settings/localeSetting.ts
+++ src/settings/localeSetting.ts
@@ -1,9 +1,9 @@
-import type { DropMenu } from '../components/Dropdown';
+import type { DropMenu } from '@/components/Dropdown';
 import type { LocaleSetting, LocaleType } from '#/config';
 
 export const LOCALE: { [key: string]: LocaleType } = {
   ZH_CN: 'zh_CN',
-  EN_US: 'en',
+  EN_US: 'en_US',
 };
 
 export const localeSetting: LocaleSetting = {
