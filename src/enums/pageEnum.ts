b06416e9a2693bc64031d58be50643b72fba4db9
yihhao wang <2633565580@qq.com>
2024-07-11 02:01:13 +0800

    feat(page enum): record pages routes as page enum

--- src/enums/pageEnum.ts
+++ src/enums/pageEnum.ts
@@ -2,13 +2,28 @@ export enum PageEnum {
   // basic login path
   BASE_LOGIN = '/login',
   // basic home path
-  BASE_HOME = '/dashboard',
+  BASE_HOME = '/dashboard/workbench',
   // error page path
   ERROR_PAGE = '/exception',
   // project about
   ABOUT_PAGE = '/about',
   // error log page path
   ERROR_LOG_PAGE = '/error-log/list',
+
+  // api key page
+  APIKEY_PAGE = '/apikey',
+
+  // node pages
+  NODE_INDEX_PAGE = '/node/',
+
+  // server pages
+  SERVER_INDEX_PAGE = '/server/',
+
+  // system pages
+  SYSTEM_USER_PAGE = '/sys/user',
+  SYSTEM_ROLE_PAGE = '/sys/role',
+  SYSTEM_CRON_PAGE = '/sys/cron',
+  SYSTEM_CONFIG_PAGE = '/sys/config',
 }
 
 export const PageWrapperFixedHeightKey = 'PageWrapperFixedHeight';
